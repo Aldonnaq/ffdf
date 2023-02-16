6a1de8f5c28f3a76b1f58a337984d3d7f1eea21f
Aldonnaq <123084115+Aldonnaq@users.noreply.github.com>
2023-02-17 00:51:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import telebot
-from googletrans import Translator
 
 # Set up the Telegram bot
 bot = telebot.TeleBot("6271347286:AAF-Y9N-C3YItKZwmxiGSwwqMY0-k0wPcVk")
 
-# Set up the translator
-translator = Translator()
-
-# Define a function to handle all messages
-@bot.message_handler(func=lambda message: True)
-def translate_message(message):
-    # Translate the message to Arabic
-    translated_text = translator.translate(message.text, dest='ar').text
-    # Send the translated message back
-    bot.reply_to(message, translated_text)
+# Define a function to handle the /start command
+@bot.message_handler(commands=['start'])
+def start(message):
+    # Send a "hi" message back
+    bot.send_message(message.chat.id, "Hi!")
 
 # Start the bot
 bot.polling()
